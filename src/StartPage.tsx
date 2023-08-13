ea15c037abb6bea2f9c18744b875845516a5ae2e
Chase <chaseburkdoll@gmail.com>
2023-08-13 14:10:17 -0500

    Beginning of the start page

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -1,7 +1,15 @@
 import "./StartPage.css";
 //use sass???
+//after today will change
 const StartPage = () => {
-  return <div className="start__container"></div>;
+  return (
+    <div className="start__container">
+      <div className="start__blackMarble">
+        <h1 className="start__title">Aggravation</h1>
+      </div>
+      <div className="start__red start__marble"></div>
+    </div>
+  );
 };
 
 export default StartPage;
