93a737f868ed1891ca20644f2bbc4ba2d1b9067c
Chase <chaseburkdoll@gmail.com>
2023-08-13 14:28:52 -0500

    tiny marble styling

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -7,7 +7,10 @@ const StartPage = () => {
       <div className="start__blackMarble">
         <h1 className="start__title">Aggravation</h1>
       </div>
+      <div className="start__green start__marble"></div>
       <div className="start__red start__marble"></div>
+      <div className="start__blue start__marble"></div>
+      <div className="start__grey start__marble"></div>
     </div>
   );
 };
