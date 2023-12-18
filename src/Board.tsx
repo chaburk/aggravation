003e41b563aea17ce38020d5c61133e5cc4982c6
Chase <chaseburkdoll@gmail.com>
2023-12-18 10:52:23 -0600

    Visual updates

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,6 +2,8 @@ import "./Board.css";
 import Marble from "./Marble";
 import Player from "./Player";
 
+//create an updateboard function.
+
 interface Space {
   id: number;
   type: "board" | "marble";
@@ -12,7 +14,7 @@ interface Space {
 //112 middle
 const spaces: React.ReactNode[] = [];
 
-const spacesToKeep: int = [
+const spacesToKeep: number[] = [
   5, 6, 7, 8, 9, 14, 16, 20, 22, 24, 28, 32, 35, 37, 39, 42, 48, 50, 52, 54, 56,
   64, 65, 67, 69, 70, 75, 76, 77, 78, 79, 80, 82, 84, 85, 86, 87, 88, 89, 90,
   104, 105, 106, 107, 108, 109, 110, 114, 115, 116, 117, 118, 119, 120, 112,
@@ -20,42 +22,41 @@ const spacesToKeep: int = [
   157, 159, 170, 172, 174, 185, 187, 189, 200, 202, 204, 215, 216, 217, 218,
   219,
 ];
+
+const board: number[] = [];
 //playerthreewin 105, 106, 107, 108, 109, 110,
 //playerfourwin 114, 115, 116, 117, 118, 119,
-//for the win, i can render smaller ones? keep same outside size but render smaller
-//can do the same for the marbles because it looks bad
-//how should i do this? should i create a dictionary to map them so inside the game its just 1-n
-const playerOneMarbles: int = [0, 16, 32, 48, 64];
-const playerOneWin: int = [7, 23, 37, 52, 67, 82];
-const playerTwoMarbles: int = [14, 28, 42, 56, 70];
-const playerTwoWin: int = [142, 157, 172, 187, 202, 217];
-const playerThreeMarbles: int = [210, 196, 182, 168, 154];
-const playerFourMarbles: int = [224, 208, 192, 176, 160];
+
+const playerOneMarbles: number[] = [0, 16, 32, 48, 64];
+const playerOneWin: number[] = [7, 23, 37, 52, 67, 82];
+const playerTwoMarbles: number[] = [14, 28, 42, 56, 70];
+const playerTwoWin: number[] = [142, 157, 172, 187, 202, 217];
+const playerThreeMarbles: number[] = [210, 196, 182, 168, 154];
+const playerFourMarbles: number[] = [224, 208, 192, 176, 160];
 
 function createSpaces() {
   for (let i = 0; i < 225; i++) {
     if (playerOneMarbles.includes(i)) {
       spaces.push(
-        <div className={`space grid-item playerOne`} key={i}>
+        <div className={`space marble`} key={i}>
           <Marble marbleColor={"red"} />
         </div>
       );
     } else if (playerTwoMarbles.includes(i)) {
       spaces.push(
-        <div className={`space grid-item playerTwo`} key={i}>
+        <div className={`space marble`} key={i}>
           <Marble marbleColor={"blue"} />
         </div>
       );
     } else if (playerThreeMarbles.includes(i)) {
       spaces.push(
-        //<Marble />
-        <div className={`space playerThree`} key={i}>
+        <div className={`space marble`} key={i}>
           <Marble marbleColor={"green"} />
         </div>
       );
     } else if (playerFourMarbles.includes(i)) {
       spaces.push(
-        <div className={`space playerFour`} key={i}>
+        <div className={`space marble`} key={i}>
           <Marble marbleColor={"purple"} />
         </div>
       );
@@ -68,6 +69,7 @@ function createSpaces() {
 }
 
 createSpaces();
+console.log(spacesToKeep.length);
 
 const Board = () => {
   //Logic of the game will be in this file?
