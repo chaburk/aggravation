e0b5fba39c53287f346131738b882984f74188b2
Chase <chaseburkdoll@gmail.com>
2023-12-19 11:37:16 -0600

    Finished Die component

--- src/Die.tsx
+++ src/Die.tsx
@@ -1,22 +1,50 @@
 import "./Die.css";
 import { useState } from "react";
 
+type DotPatterns = {
+  [key in "1" | "2" | "3" | "4" | "5" | "6"]: string;
+};
+
 const randomNum = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
 
 const Die = () => {
-  const [roll, setRoll] = useState(0);
+  const [dots, setDots] = useState([
+    <div className={`dice-dot dice-one`} key={0}></div>,
+  ]);
+
+  const dotPatterns: DotPatterns = {
+    "1": "dice-one",
+    "2": "dice-two",
+    "3": "dice-three",
+    "4": "dice-four",
+    "5": "dice-five",
+    "6": "dice-six",
+  };
+
   const diceRoll = () => {
     for (let i = 0; i < 10; i++) {
       setTimeout(() => {
-        setRoll(randomNum());
+        const ranNum = randomNum();
+        const tempDot = [];
+        for (let i = 0; i < ranNum; i++) {
+          tempDot.push(
+            <div
+              className={`dice-dot ${
+                dotPatterns[ranNum.toString() as keyof DotPatterns]
+              }`}
+              key={i}
+            ></div>
+          );
+        }
+        setDots(tempDot);
       }, i * 100);
     }
   };
   return (
-    <div className="die-container" onClick={diceRoll}>
-      <div className="number">{roll}</div>
+    <div className={`die-container`} onClick={diceRoll}>
+      {dots}
     </div>
   );
 };
